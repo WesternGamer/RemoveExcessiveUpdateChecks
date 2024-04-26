8653a40883e4a2a7118dbdde87a990463dc143f2
WesternGamer <Kevnguyen474@gmail.com>
2024-04-25 21:31:53 -0400

    Initial Setup

--- ClientPlugin/Properties/AssemblyInfo.cs
+++ ClientPlugin/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PluginTemplate")]
+[assembly: AssemblyTitle("RemoveExcessiveUpdateChecks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PluginTemplate")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("WesternGamer")]
+[assembly: AssemblyProduct("RemoveExcessiveUpdateChecks")]
+[assembly: AssemblyCopyright("Copyright © WesternGamer 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("A061FC6C-713E-42CD-B413-151AC8A5074C")]
+[assembly: Guid("D601C95E-6110-4672-B14C-DC65190CA385")]
 
 // Version information for an assembly consists of the following four values:
 //
